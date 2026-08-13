8b7e6abcc7d4c3383b85ed8eba52b8ef1938cfda
NaveenDevHub <zynkcodesolutions@gmail.com>
2026-08-13 15:30:57 +0530

    2nd task 2nd commit

--- task2.js
+++ task2.js
@@ -1,10 +1,18 @@
-var box1 = document.getElementById("inputnum1")
-var box2 = document.getElementById("inputnum2")
-var result = document.getElementById("result")
-function resultfun()
-{
-    var box1value = Number(box1.value)
-    var box2value = Number(box2.value)
-    var total = box1value + box2value
-    result.textContent= (`Result : ${total}`)
-}
+// method 1
+// var box1 = document.getElementById("inputnum1")
+// var box2 = document.getElementById("inputnum2")
+// var result = document.getElementById("result")
+// function resultfun()
+// {
+//     var box1value = Number(box1.value)
+//     var box2value = Number(box2.value)
+//     var total = box1value + box2value
+//     result.textContent= (`Result : ${total}`)
+// }
+
+// method 2
+
+let value1 = Number(window.prompt("Enter the value 1"));
+let value2 = Number(window.prompt("Enter the value 2"));
+let result = value1+ value2;
+console.log(result)
